e7a304a8ff5c0cf1c397e3df89deccae37338027
hiyongz <zhiyo2016@163.com>
2021-08-16 18:46:13 +0800

    update

--- src/testGo/demo.go
+++ src/testGo/demo.go
@@ -15,4 +15,9 @@ func main() {
 	var value2 = 3
 	sum := add(value1,value2)
 	fmt.Printf("%d + %d = %d",value1,value2,sum)
+
+	a := `"dev_info"`
+	b := []byte{`"dev_info"`}
+	fmt.Printf("%s",a)
+	fmt.Printf("%s",b[0])
 }
