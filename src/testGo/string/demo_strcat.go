9940c2128e40d8fa21d03472dd62c2975b62a402
hiyongz <zhiyo2016@163.com>
2021-09-01 07:43:20 +0800

    update

--- src/testGo/string/demo_strcat.go
+++ src/testGo/string/demo_strcat.go
@@ -19,9 +19,11 @@ func DemoBytes() {
 func DemoStrings() {
 	var builder1 strings.Builder
 	builder1.WriteString("hello")
+	// fmt.Printf("The length 0f builder1: %d\n", builder1.Len())
 	builder1.WriteByte(' ')
 	builder1.WriteString("world !")
 	builder1.Write([]byte{'\n'})
+
 	fmt.Println(builder1.String())
 	fmt.Printf("%T",builder1.String())
 	// builder1.Reset()
@@ -29,7 +31,7 @@ func DemoStrings() {
 
 	f1 := func(b strings.Builder) {
 		// b.WriteString("world !")
-		fmt.Println(b.String())
+		// fmt.Println(b.String())
 	}
 	f1(builder1)
 
