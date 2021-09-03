852e0090c0d39433f680722cc618dd3e7112080e
hiyongz <zhiyo2016@163.com>
2021-09-03 17:45:22 +0800

    update demo_strcat.go

--- src/testGo/string/demo_strcat.go
+++ src/testGo/string/demo_strcat.go
@@ -3,12 +3,13 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"strings"
 )
 
 func DemoBytes() {
-	var buffer bytes.Buffer	
-	buffer.WriteString("hello ")		
+	var buffer bytes.Buffer
+	buffer.WriteString("hello ")
 	buffer.WriteString("world !")
 	fmt.Printf("The length of buffer: %d\n", buffer.Len())
 	fmt.Printf("The capacity of buffer: %d\n", buffer.Cap())
@@ -23,7 +24,6 @@ func DemoBytes() {
 	fmt.Printf("The length of buffer: %d\n", buffer.Len())
 	fmt.Printf("The capacity of buffer: %d\n", buffer.Cap())
 
-
 }
 
 func DemoStrings() {
@@ -33,13 +33,8 @@ func DemoStrings() {
 	builder1.WriteByte(' ')
 	builder1.WriteString("world")
 	builder1.Write([]byte{' ', '!'})
-	fmt.Println(builder1.String())	
+	fmt.Println(builder1.String())
 
-	reader1 := strings.NewReader("hello world!")
-	buf1 := make([]byte, 5)
-	n, _ := reader1.Read(buf1)
-	fmt.Printf("%d bytes were read. (call Read)\n", n)
-	fmt.Println(string(buf1))
 	f1 := func(b strings.Builder) {
 		// b.WriteString("world !")  //会报错
 	}
@@ -48,8 +43,36 @@ func DemoStrings() {
 	builder1.Reset()
 	fmt.Printf("The length of builder1: %d\n", builder1.Len())
 
-}
+	reader1 := strings.NewReader("hello world!")
+	buf1 := make([]byte, 5)
+	n, _ := reader1.Read(buf1)
+	fmt.Println(string(buf1))
+	fmt.Printf("%d bytes were read. (call Read)\n", n)
 
+	// 示例2。
+	buf2 := make([]byte, 6)
+	offset1 := int64(6)
+	n, _ = reader1.ReadAt(buf2, offset1)
+	fmt.Printf("%d bytes were read. (call ReadAt, offset: %d)\n", n, offset1)
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
+	fmt.Println(string(buf2))
+	fmt.Println()
+
+	// 示例3。
+	offset2 := int64(6)
+	expectedIndex := reader1.Size() - int64(reader1.Len()) + offset2
+	fmt.Printf("Seek with offset %d and whence %d ...\n", offset2, io.SeekCurrent)
+	readingIndex, _ := reader1.Seek(offset2, io.SeekCurrent)
+	fmt.Printf("The reading index in reader: %d (returned by Seek)\n", readingIndex)
+	fmt.Printf("The reading index in reader: %d (computed by me)\n", expectedIndex)
+
+	n, _ = reader1.Read(buf2)
+	fmt.Printf("%d bytes were read. (call Read)\n", n)
+	fmt.Printf("The reading index in reader: %d\n",
+		reader1.Size()-int64(reader1.Len()))
+	fmt.Println(string(buf2))
+}
 
 func main() {
 	// DemoBytes()
