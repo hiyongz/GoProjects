fdb110a4d81de3df2dd1f815dfcd0af6f07c3c7a
hiyongz <zhiyo2016@163.com>
2021-09-07 07:34:22 +0800

    update channel

--- src/testGo/channel/channel_block2.go
+++ src/testGo/channel/channel_block2.go
@@ -2,31 +2,22 @@ package main
 
 import (
     "fmt"
-    // "time"
 )
 
 func main() {
-    //创建一个无缓存的channel
-    ch := make(chan int, 0)
+    str1 := []string{"hello","world", "!"}
+	ch1 := make(chan string, 0)
 
-    //len(ch)缓冲区剩余数据个数， cap(ch)缓冲区大小，两者这里永远都是0
-    fmt.Printf("len(ch) = %d, cap(ch)= %d\n", len(ch), cap(ch))
-
-    //新建协程
     go func() {
-        for i := 0; i < 3; i++ { //写三次
-            fmt.Printf("子协程：i = %d\n", i)
-            ch <- i //往chan写内容
-            fmt.Printf("len(ch) = %d, cap(ch)= %d\n", len(ch), cap(ch))
+        for _, str := range str1 {
+            ch1 <- str
         }
+        close(ch1)
     }()
 
-    //延时2秒
-    // time.Sleep(2 * time.Second)
-
-    for i := 0; i < 3; i++ { //必须读三次
-        num := <-ch //读管道中内容，没有内容前，阻塞
-        fmt.Println("num = ", num)
-    }
+	for i := 0; i < len(str1); i++ {
+		elem := <- ch1
+		fmt.Println(elem)
+	}
 
 }
