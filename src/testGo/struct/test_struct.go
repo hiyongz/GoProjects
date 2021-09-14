068ce91532c3e14c45cae8d3900cc4dd75cf03d9
hiyongz <zhiyo2016@163.com>
2021-09-14 17:47:45 +0800

    update test_struct.go add demo_waitgroup0.go

--- src/testGo/struct/test_struct.go
+++ src/testGo/struct/test_struct.go
@@ -2,64 +2,32 @@ package main
 
 import "fmt"
 
-// 示例1。
-// AnimalCategory 代表动物分类学中的基本分类法。
-type AnimalCategory struct {
-	kingdom string // 界。
-	phylum  string // 门。
-	class   string // 纲。
-	order   string // 目。
-	family  string // 科。
-	genus   string // 属。
-	species string // 种。
+type Skills struct {
+	speak string 
 }
 
-func (ac AnimalCategory) String() string {
-	return fmt.Sprintf("%s%s%s%s%s%s%s",
-		ac.kingdom, ac.phylum, ac.class, ac.order,
-		ac.family, ac.genus, ac.species)
+func (s Skills) Speak()  {
+	fmt.Println(s.speak)	
 }
 
-// 示例2。
-type Animal struct {
-	scientificName string // 学名。
-	AnimalCategory        // 动物基本分类。
+type Robot struct {
+	name string // 姓名
+	height int // 身高
+	Skills
 }
 
-// 该方法会"屏蔽"掉嵌入字段中的同名方法。
-func (a Animal) String() string {
-	return fmt.Sprintf("%s (category: %s)",
-		a.scientificName, a.AnimalCategory)
-}
-
-// 示例3。
-type Cat struct {
-	name string
-	Animal
-}
-
-// 该方法会"屏蔽"掉嵌入字段中的同名方法。
-func (cat Cat) String() string {
-	return fmt.Sprintf("%s (category: %s, name: %q)",
-		cat.scientificName, cat.Animal.AnimalCategory, cat.name)
+func (r Robot) Speak() {	
+	fmt.Printf("My name is %s, ",r.name)
+	r.Skills.Speak()
 }
 
 func main() {
-	// 示例1。
-	category := AnimalCategory{species: "cat"}
-	fmt.Printf("The animal category: %s\n", category)
-
-	// 示例2。
-	animal := Animal{
-		scientificName: "American Shorthair",
-		AnimalCategory: category,
+	skill := Skills{speak: "hello !"}
+	skill.Speak()
+	
+	robot := Robot{
+		name: "Optimus Prime",
+		Skills: skill,
 	}
-	fmt.Printf("The animal: %s\n", animal)
-
-	// 示例3。
-	cat := Cat{
-		name:   "little pig",
-		Animal: animal,
-	}
-	fmt.Printf("The cat: %s\n", cat)
+	robot.Speak()
 }
